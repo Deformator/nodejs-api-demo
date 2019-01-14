702ede339b3944b77b7dd590e333eb0d0692ef14
Andrii Damm <andrii@unionms-iMac.local>
2019-01-14 12:30:44 -0500

    Setup separate test and develpment envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyPraser = require('body-parser');
@@ -8,12 +10,11 @@ var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyPraser.json());
 
 app.post('/todos', (req, res) => {
-    console.log(req);
     var todo = new Todo({
         text: req.body.text
     });
